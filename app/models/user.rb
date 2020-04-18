f0f94ab90ec709494a3e3087923f158ada585547
freshmurry <lawrencemurry@yahoo.com>
2020-04-18 06:06:47 +0000

    Added username to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   validates :username, presence: true, length: {maximum: 50}
   
   # def total_followers
-  #   "#{first_name} #{last_name}"
+  #   "#{username}"
   # end
   
   # def total_following
@@ -26,6 +26,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
+      user.username = auth.info.username
       user.image = auth.info.image.gsub!("_normal", "") # assuming the user model has an image
       user.uid = auth.uid
       user.provider = auth.provider
