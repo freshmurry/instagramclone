693e662a662a75039213566a9d105ed97bc20916
freshmurry <lawrencemurry@yahoo.com>
2020-05-16 07:52:53 +0000

    Working to get photo to show on users page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,9 @@ module ApplicationHelper
   def avatar(user)
     if user.image
       "http://graph.facebook.com/#{user.uid}/picture?type=large"
-    elsif
+    else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
-    else
-      imgage(user)
     end
   end
 end
