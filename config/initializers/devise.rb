0f04bd0da36e4e73c965a87527deea83178c9a5d
freshmurry <lawrencemurry@yahoo.com>
2021-03-13 20:34:34 +0000

    Commented out mobile screenshots of IG and fixed Facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -277,7 +277,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   # config.omniauth :twitter, ENV["twitter_app_id"], ENV["twitter_app_secret"]
-  config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"]
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: 'https://instafakeapp.herokuapp.com/auth/facebook/callback'
-
+  # config.omniauth :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"]
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'email', info_fields: 'email, name'
+  # config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: 'https://instafakeapp.herokuapp.com/auth/facebook/callback'
 end
